5dbaf0589a356c35d22f8381ef611f0acc9c628b
vividmint <vividmint@qq.com>
2017-08-02 19:23:29 +0800

    lalala

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var http = require("http");
 var url = require("url");
 
+var token = "345452566:AAFU0_F0sGnIrrUHBqrdr51WdZrtrhQJXwk";
+
 function start() {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
@@ -16,7 +18,19 @@ function start() {
         }).on('end', () => {
             body = Buffer.concat(body).toString();
             // at this point, `body` has the entire request body stored in it as a string
-            console.log('body',body)
+            console.log('body', body);
+            var obj = body.message;
+            console.log(obj)
+            // var id = body.message.chat.id;
+            // var text = body.message.text;
+            // var result = /hi/.test(text);
+            // var reply = "你好，全世界最可爱的宝宝";
+            // if (result) {
+            //     request({
+            //         url: `https://api.telegram.org/bot
+            //     ${token}/sendMessage?chat_id=${id}&text=${reply}`
+            //     })
+            // }
         });
         response.write("Hello World");
         response.end();
@@ -27,3 +41,68 @@ function start() {
 }
 
 start();
+
+function request(params) {
+    let url = params.url;
+    let method;
+    if (!params.method) {
+        method = 'GET';
+    } else {
+        method = params.method;
+    }
+    var obj = {
+        method: params.method,
+        mode: 'cors'
+    };
+    if (method == 'POST' || method == 'DELETE') {
+        obj.body = JSON.stringify(params.body);
+    }
+    if (false) {
+        return fetch(url, obj).then(function(response) {
+            //console.log(response);
+            if (response.status >= 200 && response.status < 300) {
+                return response.json();
+            } else {
+                return response.json();
+            }
+        }, function(err) {
+            console.log(err);
+            return Promise.reject(err);
+        });
+    } else {
+        return new Promise(function(resolve, reject) {
+            // console.log(url);
+            var httpRequest = new XMLHttpRequest();
+            httpRequest.onreadystatechange = function() {
+                if (httpRequest.readyState === 4) {
+                    if (httpRequest.status === 200) {
+                        try {
+                            // console.log(httpRequest.response);
+                            var data = JSON.parse(httpRequest.response);
+                            resolve(data);
+                        } catch (e) {
+                            reject('json解析错误');
+                        }
+
+                    } else {
+                        var e = JSON.parse(httpRequest.response);
+                        console.log(e);
+                        reject(e);
+                    }
+                }
+
+            };
+            httpRequest.open(method, url, true);
+            if (method === 'GET') {
+                httpRequest.send(null);
+            } else if (method === 'POST' || method === 'DELETE' || method === 'PUT') {
+                httpRequest.setRequestHeader('Content-Type', 'application/json');
+                httpRequest.send(obj.body);
+            }
+
+        });
+    }
+}
+
+// ps aux|grep node
+// kill 2343
---
+++ test.js
@@ -0,0 +1,22 @@
+{
+    "update_id": 685755613,
+    "message": {
+        "message_id": 10,
+        "from": {
+            "id": 399350232,
+            "first_name": "\u5356\u6c14",
+            "last_name": "\u7403\u7684",
+            "username": "vividmint",
+            "language_code": "zh-Hans-GI"
+        },
+        "chat": {
+            "id": 399350232,
+            "first_name": "\u5356\u6c14",
+            "last_name": "\u7403\u7684",
+            "username": "vividmint",
+            "type": "private"
+        },
+        "date": 1501615657,
+        "text": "\u8fa3\u9e21\u6768\u56fd\u5b9d"
+    }
+}
