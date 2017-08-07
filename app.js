c67ca6cce6cd3d370bc92274e2db29dfa99da828
vividmint <vividmint@qq.com>
2017-08-08 01:35:07 +0800

    login

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 "use strict";
+const AV = require('leancloud-storage');
+
+AV.init({
+    appId: "fwNTEgKvtqXmGdin8NH3k6nR-gzGzoHsz",
+    appKey: "6zhh6evebN7hiOMazqv6lnbN"
+});
 var http = require("http");
 var url = require("url");
 var request = require('./request.js');
+
+const user = AV.User.current();
+
+
 var ENV = process.env.NODE_ENV;
 var token = "345452566:AAFU0_F0sGnIrrUHBqrdr51WdZrtrhQJXwk";
 console.log(ENV)
-// var a = {ba:1,c:2}
-// const {ba} = a;
+
+// function getMovieData(params) {
+//     var query = new AV.Query('Todo');
+//     let objectId;
+//     if (params.fromId) {
+//         objectId = params.fromId;
+//     } else {
+//         // objectId = lastViewId;
+//     }
+//     query.get(objectId).then(function(todo) {
+//
+//     }, function(error) {
+//         // 异常处理
+//     });
+// }
+var buttons = {};
+buttons.like = "like";
+buttons.unlike = "nope";
+var data = {
+    userInfo: {},
+    idSets: null,
+    listArr: null,
+    likeArr: [],
+    unlikeArr: [],
+    movieData: {}
+};
+
 function start() {
     function onRequest(req, response) {
         var pathname = url.parse(req.url).pathname;
@@ -20,17 +55,21 @@ function start() {
         }).on('end', () => {
             body = Buffer.concat(body).toString();
             var obj = JSON.parse(body);
-            var id = obj.message.chat.id;
+            var chatId = obj.message.chat.id;
             var text = obj.message.text;
-            var reply = "hi";
-            var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${id}&text=${reply}`;
-            if (text) {
+            var uid = obj.message.chat.username;
+            toLogin(uid);
+
+
+            var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${reply}`;
+            let result = /movie/.test(text);
+            if (result) {
                 const config = {
                     url
                 }
                 console.log(ENV)
-                if(ENV !== 'production'){
-                  config.proxy = "http://127.0.0.1:1087"
+                if (ENV !== 'production') {
+                    config.proxy = "http://127.0.0.1:1087"
                 }
                 request(config).then(function(r) {
                     console.log(r)
@@ -48,7 +87,12 @@ function start() {
 
 start();
 
+function toLogin(uid) {
+    AV.User.signUpOrlogInWithAuthData({
+        'uid': uid,
+    }, 'telegram');
 
+}
 
 // ps aux|grep node
 // kill 2343
