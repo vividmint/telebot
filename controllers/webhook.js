84d82deafc55ace7f95392d1cd040765ed7ee197
vividmint <vividmint@qq.com>
2017-08-17 21:32:08 +0800

    night

--- controllers/webhook.js
+++ controllers/webhook.js
@@ -17,67 +17,64 @@ exports.telebot = async(ctx) => {
     console.log('body', obj)
     if (obj.callback_query) {
         let movieObj = JSON.parse(obj.callback_query.data);
-        if(movieObj.type==='like'){
-          let movieId = movieObj.movieId;
-          let _date = new Date();
-          let _createdAt = _date.getTime();
+        if (movieObj.type === 'like') {
+            let movieId = movieObj.movieId;
+            let _date = new Date();
+            let _createdAt = _date.getTime();
 
-          var insertLikeIdString = `INSERT INTO likeList(id,movieId,createdAt,updatedAt) VALUES(null,${movieId},${_createdAt},${_createdAt})`
+            var insertLikeIdString = `INSERT INTO likeList(id,movieId,createdAt,updatedAt) VALUES(null,${movieId},${_createdAt},${_createdAt})`;
+            var insertLikeIdResult = await mysql.query(insertLikeIdString);
+            console.log("like success");
+
         }
 
-    }
-    var thirdPartyId = obj.message.from.id;
-    var text = obj.message.text;
-    var uid = obj.message.chat.username;
-    var nickname = obj.message.chat.first_name + obj.message.chat.last_name;
-    var date = new Date();
-    var createdAt = date.getTime();
-    var reply;
+    } else {
+        var thirdPartyId = obj.message.from.id;
+        var text = obj.message.text;
+        var uid = obj.message.chat.username;
+        var nickname = obj.message.chat.first_name + obj.message.chat.last_name;
+        var date = new Date();
+        var createdAt = date.getTime();
+        var reply;
 
-    let queryUserString = `SELECT * FROM user WHERE thirdPartyId="${thirdPartyId}" and thirdPartyType="tg"`;
+        let queryUserString = `SELECT * FROM user WHERE thirdPartyId="${thirdPartyId}" and thirdPartyType="tg"`;
 
-    try {
-        var queryResultArr = await mysql.query(queryUserString);
-    } catch (e) {
-        console.log(e);
-        ctx.status = 504;
-        ctx.body = e;
-        return;
-    }
-    var lastViewId;
-    if (queryResultArr.length === 0) {
-        //new user
-        let signUpString = `INSERT INTO user(id, uid,thirdPartyId, nickname, createdAt, lastViewId,thirdPartyType) VALUES (null,"${uid}","${thirdPartyId}","${nickname}",${createdAt},1,"tg")`;
         try {
-            var signUpRusult = await mysql.query(signUpString);
-            console.log("signUpRusult", signUpRusult)
-
+            var queryResultArr = await mysql.query(queryUserString);
         } catch (e) {
             console.log(e);
             ctx.status = 504;
             ctx.body = e;
             return;
         }
-        lastViewId = 1;
-    } else {
-        //old user
-        lastViewId = queryResultArr[0].lastViewId;
-    }
-    var queryMovieString = `SELECT * FROM movie WHERE id=${lastViewId}`;
+        var lastViewId;
+        if (queryResultArr.length === 0) {
+            //new user
+            let signUpString = `INSERT INTO user(id, uid,thirdPartyId, nickname, createdAt, lastViewId,thirdPartyType) VALUES (null,"${uid}","${thirdPartyId}","${nickname}",${createdAt},1,"tg")`;
+            try {
+                var signUpRusult = await mysql.query(signUpString);
+                console.log("signUpRusult", signUpRusult)
 
+            } catch (e) {
+                console.log(e);
+                ctx.status = 504;
+                ctx.body = e;
+                return;
+            }
+            lastViewId = 1;
+        } else {
+            //old user
+            lastViewId = queryResultArr[0].lastViewId;
+        }
+
+    }
     try {
-        var movieArr = await mysql.query(queryMovieString);
-
+        var movieData = await queryMovieData(lastViewId);
     } catch (e) {
-        console.log(e);
-        ctx.status = 504;
-        ctx.body = e;
-        return;
+      console.log(e)
     }
-    var movieData = movieArr[0];
 
-    reply = `${movieData.name}\n豆瓣评分：${movieData.score}\n主演：${movieData.info}`;
-    console.log('reply', reply);
+    reply = `《${movieData.name}》\n豆瓣评分：${movieData.score}\n主演：${movieData.info}`;
 
     var url = `https://api.telegram.org/bot${TELEBOT_TOKEN}/sendMessage`;
     let result = /movie/.test(text);
@@ -95,14 +92,14 @@ exports.telebot = async(ctx) => {
                                 "text": BUTTON_LIKE,
                                 "callback_data": JSON.stringify({
                                     "movieId": `${movieData.id}`,
-                                    "type":"like"
+                                    "type": "like"
                                 })
                             },
                             {
                                 "text": BUTTON_UNLIKE,
                                 "callback_data": JSON.stringify({
                                     "movieId": `${movieData.id}`,
-                                    "type":"nope"
+                                    "type": "nope"
                                 })
                             }
                         ]
@@ -117,7 +114,9 @@ exports.telebot = async(ctx) => {
         }
         try {
             var tgResult = await request(config);
-            console.log("tgResult", tgResult)
+            console.log("tgResult", tgResult);
+
+            var setLastViewId = `UPDATE user SET lastViewId=${lastViewId+1} WHERE uid=${uid}`
 
         } catch (e) {
             ctx.status = 500;
@@ -128,4 +127,20 @@ exports.telebot = async(ctx) => {
         ctx.body = tgResult;
     }
 
+}
+
+async function queryMovieData(id) {
+    var queryMovieString = `SELECT * FROM movie WHERE id=${id}`;
+
+    try {
+        var movieArr = await mysql.query(queryMovieString);
+
+    } catch (e) {
+        console.log(e);
+        // ctx.status = 504;
+        // ctx.body = e;
+        return Promise.reject(e);
+    }
+    var movieData = movieArr[0];
+    return movieData;
 }
