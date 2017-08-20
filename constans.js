781046675bc45af112ab1387036357b350a43a03
vividmint <vividmint@qq.com>
2017-08-20 16:46:26 +0800

    rename

--- constans.js
+++ constans.js
@@ -1,5 +1,3 @@
-exports.TELEBOT_TOKEN = "345452566:AAFU0_F0sGnIrrUHBqrdr51WdZrtrhQJXwk";
+exports.TELEBOT_TOKEN = "412288859:AAEvdYtx6hPNKZqoZ0OD8__893t-bNnujFc";
 exports.BUTTON_LIKE = "like";
 exports.BUTTON_UNLIKE = "nope";
-exports.LEANCLOUD_APP_ID = "fwNTEgKvtqXmGdin8NH3k6nR-gzGzoHsz";
-exports.LEANCLOUD_APP_KEY = "6zhh6evebN7hiOMazqv6lnbN";
